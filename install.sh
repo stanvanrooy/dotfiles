5aa8a63000bf8a82ac3a8156a8cd816dcd65f9c1
Stan van Rooy <stan@rooy.dev>
2021-09-02 10:57:38 +0200

    update installs cript

--- install.sh
+++ install.sh
@@ -17,6 +17,13 @@ sudo apt install nodejs -y
 rm "$HOME/.vimrc"
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 
+# install go
+sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
+
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
@@ -27,7 +34,13 @@ ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
+# build coc.nvim
+cd $HOME/.vim/bundle/coc.nvim/
+sudo npm i -g yarn
+yarn install
+yarn build
+
 # set up guake
 guake --restore-preferences $HOME/dotfiles/.guake
-source $HOME/.bashrc
+source $HOME/dotfiles/.bashrc
 
